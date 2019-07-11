c27d5169e868bfa91158789c3a039921a30f884b
Nafi <paradoxnafi>
2019-07-11 09:36:31 -0700

    Initila commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+# Basic requirments
+os.system('apt update && apt install python3 python3-dev python3-pip wget mpg123 -y')
+
+# Talkey module for text to speech
+os.system('pip3 install speech_recognition youtube-dl')
+
