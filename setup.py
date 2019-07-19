c44309aa54962c13221b745bda25b388d68bffe9
Md. Nafi shahriyar <32407364+paradoxnafi@users.noreply.github.com>
2019-07-19 11:07:38 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-# Basic requirments
-os.system('apt update && apt install python3 python3-dev python3-pip wget mpg123 ffmpeg-y')
-
-# Talkey module for text to speech
-os.system('pip3 install beautifulsoup4 youtube-dl')
-
